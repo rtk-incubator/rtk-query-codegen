72d4cdfa3d4d00e31675c3a240f70c5fc51d50de
Lenz Weber <mail@lenzw.de>
2021-01-11 23:14:23 +0100

    simple string snapshot serializer

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -5,3 +5,10 @@ const { server } = require('./mocks/server');
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+expect.addSnapshotSerializer({
+  test: (val) => typeof val === 'string',
+  print: (val) => {
+    return val as string;
+  },
+});
