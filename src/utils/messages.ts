5561519ff323fc4666e41b9a07e576343651fc1d
kahirokunn <okinakahiro@gmail.com>
2021-01-19 11:25:29 +0900

    add error message

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -2,5 +2,6 @@ export const MESSAGES = {
   NAMED_EXPORT_MISSING: `You specified a named export that does not exist or was empty.`,
   DEFAULT_EXPORT_MISSING: `Specified file exists, but no default export was found for the --baseQuery`,
   FILE_NOT_FOUND: `Unable to locate the specified file provided to --baseQuery`,
+  URL_NOT_FOUND: `Unable to fetch the specified url provided to --baseQuery`,
   TSCONFIG_FILE_NOT_FOUND: `Unable to locate the specified file provided to -c, --config`,
 };
